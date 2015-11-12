2d9fdf2427d0d6b040fd48af982577d45ebb009c
SBeresnev <ya.beresnev@ya.ru>
2015-11-12 17:40:14 +0300

    friday

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,11 +17,48 @@ var app = angular.module('treeViewApp', [
     'ngSanitize',
     'ngTouch',
     'angularBootstrapNavTree',
+    'ngDropdowns',
 	  'kendo.directives'
-    ])
-	.constant('DOMAIN', "" + window.location.protocol + '//'+ window.location.hostname+":8080")
+    ]).directive('bootstrapDropdown', function ($compile) {
+  return {
+    restrict: 'E',
+    scope: {
+      items: '=dropdownData',
+      selectedItem: '=preselectedItem',
+      ngModel: '=',
+      placeholder: '@',
+      fieldLabel: '@'
+    },
+    link: function (scope, element, attrs) {
+
+      scope.selectVal = function (item) {
+        scope.ngModel = item;
+      };
+
+      var html = '';
+      html += '<div class="form-group">';
+      html += '  <div class="input-group">';
+      html += '    <div class="input-group-addon" style="min-width:100px" data-ng-model="fieldLabel">{{fieldLabel}}</div>';
+      html += '    <input class="form-control" id="statusinput" type="text" data-ng-model ="ngModel" data-ng-attr-placeholder="{{placeholder}}" />';
+      html += '    <div class="input-group-btn" data-ng-if="items.length">';
+      html += '      <div class="btn-group ">';
+      html += '        <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown" >';
+      html += '          <span class="caret"></span>';
+      html += '        </button>';
+      html += '        <ul class="dropdown-menu dropdown-menu-right dropdown-scroll" role="menu">';
+      html += '          <li data-ng-repeat="item in items"><a data-ng-href="" role="menuitem" tabindex="-1" data-ng-click="selectVal(item)">{{item}}</a></li>';
+      html += '        </ul>';
+      html += '      </div>';
+      html += '    </div>';
+      html += '  </div>';
+      html += '</div>';
+
+      element.append($compile(html)(scope));
+    }
+  };
+}).constant('DOMAIN', "" + window.location.protocol + '//'+ window.location.hostname+":8080")
   .constant('DOMAIN_', "" + window.location.protocol + '//'+ window.location.hostname+":9000")
-    .config(function ($routeProvider, $httpProvider) {
+  .config(function ($routeProvider, $httpProvider) {
         $routeProvider
 		.when('/main', {
         templateUrl: 'views/main.html',
--- app/scripts/controllers/address.js
+++ app/scripts/controllers/address.js
@@ -1,6 +1,7 @@
 'use strict';
 
-angular.module("treeViewApp").controller("TreeController",  function($scope, $http, DOMAIN_) {
+angular.module("treeViewApp").controller("TreeController",  function($scope, $http, DOMAIN_)
+{
 
   $scope.mywindow = [];
 
@@ -12,10 +13,182 @@ angular.module("treeViewApp").controller("TreeController",  function($scope, $ht
 
   $scope.toSend_old = {address_id:'', adr_num: '', soato:'', adr :'' };
 
+  $scope.form = {};
+  $scope.form.serviceLevelName = 'Normal';
+
+  $scope.servicesData = [
+      "Low",
+      "Normal",
+      "Medium",
+      "High",
+      "Immediate"
+    ];
+
+
   $scope.init = function() {
 
-
   }
+
+
+  $scope.ddSelectOptions = [
+    {
+      text: 'Label',
+      divider: true
+    }, {
+      text: 'Option1',
+      value: 'one',
+      iconCls: 'someicon'
+    }, {
+      text: 'Option2',
+      someprop: 'somevalue'
+    }, {
+      divider: true
+    }, {
+      text: 'Option4',
+      href: 'http://www.google.com'
+    }
+  ];
+
+  $scope.ddSelectSelected = {
+    text: "Select an Option"
+  };
+
+
+  var apple_selected, tree, treedata_avm, treedata_geography;
+
+  $scope.my_tree_handler = function(branch) {
+    var _ref;
+    $scope.output = "You selected: " + branch.label;
+    if ((_ref = branch.data) != null ? _ref.description : void 0) {
+      return $scope.output += '(' + branch.data.description + ')';
+    }
+  };
+
+  apple_selected = function(branch) {
+    return $scope.output = "APPLE! : " + branch.label;
+  };
+
+  treedata_avm = [
+    {
+      label: 'Animal',
+      children: [
+        {
+          label: 'Dog',
+          data: {
+            description: "man's best friend"
+          }
+        }, {
+          label: 'Cat',
+          data: {
+            description: "Felis catus"
+          }
+        }, {
+          label: 'Hippopotamus',
+          data: {
+            description: "hungry, hungry"
+          }
+        }, {
+          label: 'Chicken',
+          children: ['White Leghorn', 'Rhode Island Red', 'Jersey Giant']
+        }
+      ]
+    }, {
+      label: 'Vegetable',
+      data: {
+        definition: "A plant or part of a plant used as food, typically as accompaniment to meat or fish, such as a cabbage, potato, carrot, or bean.",
+        data_can_contain_anything: true
+      },
+      onSelect: function(branch) {
+        return $scope.output = "Vegetable: " + branch.data.definition;
+      },
+      children: [
+        {
+          label: 'Oranges'
+        }, {
+          label: 'Apples',
+          children: [
+            {
+              label: 'Granny Smith',
+              onSelect: apple_selected
+            }, {
+              label: 'Red Delicous',
+              onSelect: apple_selected
+            }, {
+              label: 'Fuji',
+              onSelect: apple_selected
+            }
+          ]
+        }
+      ]
+    }, {
+      label: 'Mineral',
+      children: [
+        {
+          label: 'Rock',
+          children: ['Igneous', 'Sedimentary', 'Metamorphic']
+        }, {
+          label: 'Metal',
+          children: ['Aluminum', 'Steel', 'Copper']
+        }, {
+          label: 'Plastic',
+          children: [
+            {
+              label: 'Thermoplastic',
+              children: ['polyethylene', 'polypropylene', 'polystyrene', ' polyvinyl chloride']
+            }, {
+              label: 'Thermosetting Polymer',
+              children: ['polyester', 'polyurethane', 'vulcanized rubber', 'bakelite', 'urea-formaldehyde']
+            }
+          ]
+        }
+      ]
+    }
+  ];
+
+  treedata_geography = [
+    {
+      label: 'North America',
+      children: [
+        {
+          label: 'Canada',
+          children: ['Toronto', 'Vancouver']
+        }, {
+          label: 'USA',
+          children: ['New York', 'Los Angeles']
+        }, {
+          label: 'Mexico',
+          children: ['Mexico City', 'Guadalajara']
+        }
+      ]
+    }, {
+      label: 'South America',
+      children: [
+        {
+          label: 'Venezuela',
+          children: ['Caracas', 'Maracaibo']
+        }, {
+          label: 'Brazil',
+          children: ['Sao Paulo', 'Rio de Janeiro']
+        }, {
+          label: 'Argentina',
+          children: ['Buenos Aires', 'Cordoba']
+        }
+      ]
+    }
+  ];
+
+  $scope.my_data = treedata_avm;
+
+  $scope.try_changing_the_tree_data = function() {
+    if ($scope.my_data === treedata_avm) {
+      return $scope.my_data = treedata_geography;
+    } else {
+      return $scope.my_data = treedata_avm;
+    }
+  };
+
+  $scope.my_tree = tree = {};
+
 
   function modalWin() {
     if (window.showModalDialog) {
@@ -25,8 +198,7 @@ angular.module("treeViewApp").controller("TreeController",  function($scope, $ht
     }
   }
 
-  $scope.OpenWindow= function()  // custom function on click
-  {
+  $scope.OpenWindow= function() {
 
     sessionStorage.setItem("addObj",JSON.stringify([]));
 
@@ -44,13 +216,9 @@ angular.module("treeViewApp").controller("TreeController",  function($scope, $ht
 
   };
 
-  $scope.onclick = function(){
+  $scope.onclick = function(){ }
 
-  }
-
-
-  $scope.openRef = function()
-    {
+  $scope.openRef = function() {
 
        $scope.toSend = JSON.parse(sessionStorage.getItem("addObj"));
 
